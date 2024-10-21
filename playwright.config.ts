51209de49feab5d7bdc3bfe85b957ecf13299ad0
bhoomishs <bhoomish.soni@vvdntech.in>
2024-10-21 12:12:16 +0530

    Delete playwright.config.ts

--- playwright.config.ts
+++
@@ -1,8 +0,0 @@
-// playwright.config.ts
-import { defineConfig, devices } from '@playwright/test';
-
-export default defineConfig({
-    use: {
-        headless: true, // Ensure headless mode is enabled
-    },
-});
